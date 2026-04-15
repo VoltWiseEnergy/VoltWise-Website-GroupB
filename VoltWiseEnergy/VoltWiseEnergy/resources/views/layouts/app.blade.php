da7abf8c965cf099e50fd650a6b490667489789f
MalvinAthallahsyah <malvinathallahsyah04@gmail.com>
2026-04-15 21:27:35 +0700

    VL-34 adding feature to set Theme Preference Persistence

--- VoltWiseEnergy/VoltWiseEnergy/resources/views/layouts/app.blade.php
+++ VoltWiseEnergy/VoltWiseEnergy/resources/views/layouts/app.blade.php
@@ -458,6 +458,7 @@
         const html = document.documentElement;
         const next = html.getAttribute('data-theme') === 'dark' ? 'light' : 'dark';
         html.setAttribute('data-theme', next);
+        localStorage.setItem('voltwise-theme', next);
     });
 
     @yield('scripts')
